54e75e897e61971f8af1492432da483a695605b2
Amit Chaudhary <amit@ith.tech>
2024-01-16 08:40:32 +0530

    add the light theme file

--- src/Theme/lightTheme.js
+++ src/Theme/lightTheme.js
@@ -11,7 +11,12 @@ export const lightTheme = {
         horizontalLine: '#F0F3F6',
         infoIcon: '#3F8CFF',
         infoText: '#585969',
-        heading: '#0F0B28'
+        heading: '#0F0B28',
+        text1: '#A5A6BB',
+        capInner: '#3A2CA0',
+        capOuter: '#f4f4f4',
+        progressBackground: "#EBEAF5CF"
+
         
     },
     typography: {
@@ -38,6 +43,24 @@ export const lightTheme = {
             fontWeight: '600',
             fontSize: moderateScale(12),
             lineHeight: moderateScale(18)
+        },
+        body1:{
+            fontFamily: 'Montserrat-SemiBold',
+            fontWeight: '600',
+            fontSize: moderateScale(10),
+            lineHeight: moderateScale(15)
+        },
+        inputLoanAmount: {
+            fontFamily: 'Inter-Bold',
+            fontWeight: '700',
+            fontSize: moderateScale(24),
+            lineHeight: moderateScale(26)
+        },
+        buttonTitle:{
+            fontFamily: 'Montserrat-SemiBold',
+            fontWeight: '600',
+            fontSize: moderateScale(12),
+            lineHeight: moderateScale(16)
         }
     }
 };
