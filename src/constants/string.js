6c46d9f8d25028b383d8e9040555199dbeb85b11
Amit Chaudhary <amit@ith.tech>
2024-01-16 08:44:18 +0530

    add the loan screen constant to string file

---
+++ src/constants/string.js
@@ -0,0 +1,9 @@
+module.exports = {
+    loanScreenConstants:{
+        screenTitle: 'Loan Amount',
+        loanInfo: 'Your loan amount may be split and transferred to your bank account and the hospital’s bank account.',
+        heading1: 'Select the required loan amount',
+        heading2: 'You’re Eligible for a loan of up to ',
+        toastMessage: 'Loan Amount cannot be greater than eligible limit'
+    }
+}
