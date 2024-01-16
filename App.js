6ba0988a1dd7721e0b99711a5da9b25b573b10e3
Amit Chaudhary <amit@ith.tech>
2024-01-16 08:43:08 +0530

    add the toast component to the parent

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import LoanScreen from './src/screens/LoanScreen/LoanScreen'
 import { lightTheme } from './src/Theme/lightTheme'
 import { Provider as PaperProvider } from "react-native-paper";
+import Toast from 'react-native-toast-message';
+import { moderateScale } from 'react-native-size-matters';
 
 
 export default function App() {
   return (
     <PaperProvider theme={lightTheme}>
       <LoanScreen />
+      <Toast 
+        topOffset={moderateScale(60)}/>
     </PaperProvider>
   )
 }
