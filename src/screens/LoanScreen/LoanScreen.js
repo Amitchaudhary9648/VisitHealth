efef842e0629a843b11a709c3ce36f66b93c48c9
Amit Chaudhary <amit@ith.tech>
2024-03-07 11:21:11 +0530

    added the in app review

--- src/screens/LoanScreen/LoanScreen.js
+++ src/screens/LoanScreen/LoanScreen.js
@@ -10,6 +10,7 @@ import CustomButton from '../../components/Atoms/CustomButton/CustomButton';
 import { Circle } from 'react-native-svg';
 import { loanScreenConstants } from '../../constants/string';
 import CongratsScreen from '../CongratsScreen/CongratsScreen';
+import InAppReview from 'react-native-in-app-review';
 
 
 const LoanScreen = () => {
@@ -22,6 +23,50 @@ const LoanScreen = () => {
     const minLoanAmount = 50000
     const inputRef = useRef(null);
     const [showCongrats, setShowCongrats] = useState(false)
+
+    const isAppReview = InAppReview.isAvailable();
+    console.log("is app review available", isAppReview)
+
+    const openAppReview = async () => {
+        // trigger UI InAppreview
+        InAppReview.RequestInAppReview()
+            .then((hasFlowFinishedSuccessfully) => {
+                // when return true in android it means user finished or close review flow
+                console.log('InAppReview in android', hasFlowFinishedSuccessfully);
+
+                // when return true in ios it means review flow lanuched to user.
+                console.log(
+                    'InAppReview in ios has launched successfully',
+                    hasFlowFinishedSuccessfully,
+                );
+
+                // 1- you have option to do something ex: (navigate Home page) (in android).
+                // 2- you have option to do something,
+                // ex: (save date today to lanuch InAppReview after 15 days) (in android and ios).
+
+                // 3- another option:
+                if (hasFlowFinishedSuccessfully) {
+                    // do something for ios
+                    // do something for android
+                }
+
+                // for android:
+                // The flow has finished. The API does not indicate whether the user
+                // reviewed or not, or even whether the review dialog was shown. Thus, no
+                // matter the result, we continue our app flow.
+
+                // for ios:
+                // the flow launched successfully, The API does not indicate whether the user
+                // reviewed or not, or he/she closed flow yet as android, Thus, no
+                // matter the result, we continue our app flow.
+            })
+            .catch((error) => {
+                //we continue our app flow.
+                // we have some error could happen while launching InAppReview,
+                // Check table for errors and code number that can return in catch.
+                console.log(error);
+            });
+    }
 
     useEffect(() => {
         let percentage = (loanAmount / maxLoanAmount) * 100
@@ -73,7 +118,7 @@ const LoanScreen = () => {
     });
 
     const submit = () => {
-        setShowCongrats(true)
+        openAppReview()
     }
 
 
